0bfa23f754617efd764a82980d2dcf63b114edc2
TGYK-1117 <nikolaussires@gmail.com>
2023-06-02 17:24:04 -0500

    added ruby files

---
+++ app/javascript/routes/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../components/Home";
+import Recipes from "../components/Recipes";
+import Recipe from "../components/Recipe";
+import NewRecipe from "../components/NewRecipe";
+
+
+export default (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/recipes" element={<Recipes />} />
+      <Route path="/recipe/:id" element={<Recipe />} />
+      <Route path="/recipe" element={<NewRecipe />} />
+    </Routes>
+  </Router>
+);
